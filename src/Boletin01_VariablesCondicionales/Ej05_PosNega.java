49436768f6491b5d7b4f5fd27974cbaa6c6b88d4
Barmandev <elbarmandev@gmail.com>
2023-04-06 12:44:08 +0200

    Finalizado

---
+++ src/Boletin01_VariablesCondicionales/Ej05_PosNega.java
@@ -0,0 +1,32 @@
+package Boletin01_VariablesCondicionales;
+
+import java.util.Scanner;
+
+public class Ej05_PosNega {
+
+	public static void main(String[] args) {
+	// 5. Pedir un número e indicar si es positivo o negativo.
+
+		int num1;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Vamos a mirar si un número es positivo o negativo");
+		System.out.println("Por favor introduzca el número");
+		num1 = sc.nextInt();
+
+		
+		if(num1 == 0) {
+			System.out.println("El número es cardinal");
+			
+		}else if(num1 < 0){
+			System.out.println("El número es negativo");
+		
+		}else {
+			System.out.println("Es positivo");
+		}
+		
+		
+	}
+
+}
