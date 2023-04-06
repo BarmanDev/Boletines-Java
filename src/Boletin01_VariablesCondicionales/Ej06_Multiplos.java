0369797775e5ca202bb5d8ce264d1f96b36e0c51
Barmandev <elbarmandev@gmail.com>
2023-04-06 12:48:37 +0200

    Finalizado

---
+++ src/Boletin01_VariablesCondicionales/Ej06_Multiplos.java
@@ -0,0 +1,27 @@
+package Boletin01_VariablesCondicionales;
+
+import java.util.Scanner;
+
+public class Ej06_Multiplos {
+
+	public static void main (String args[]) {
+		
+		int num1, num2;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Por favor, introduzca el primer número");
+		num1 = sc.nextInt();
+		
+		System.out.println("Por favor, introduzca el segundo número");
+		num2 = sc.nextInt();
+		
+		if(num1%num2==0) {
+			System.out.println("Son múltiplos");
+		}else{
+			System.out.println("No son múltiplos");
+		}
+		
+	}
+	
+}
